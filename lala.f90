ed1617769ab97b0f0a58624d68360faf56c566df
mcafuta <manja.cafuta@hotmail.com>
2015-08-07 11:29:04 +0200

    Added lala.f90

---
+++ lala.f90
@@ -0,0 +1,8 @@
+PROGRAM lala
+IMPLICIT NONE
+INTEGER :: hours,mins,secs,temp
+PRINT *, 'Type the values'
+READ *, hours, mins, secs
+temp = (hours*60 + mins)*60 +secs
+PRINT *, temp, 'seconds'
+END PROGRAM lala
